5603a311aab4dffbf355d8ec8eeafde55ea68078
brizism <montielbriseida@gmail.com>
2018-02-27 22:40:16 -0500

    fetch dom elements

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const mascara = document.getElementById('mascara');
+const eye_shadow = document.getElementById('eye_shadow');
+const blush = document.getElementById('blush');
+const nail_polish = document.getElementById('nail_polish');
+const foundation = document.getElementById('foundation');
+const lipstick = document.getElementById('lipstick');
+const lip_liner = document.getElementById('lip_liner');
+const bronzer = document.getElementById('bronzer');
+const eyebrows = document.getElementById('eyebrows');
+const eye_liner = document.getElementById('eye_liner');
+
+const categories = [mascara, eye_shadow, blush, nail_polish, foundation, lipstick, lip_liner, bronzer, eyebrows, eye_liner];
+
+
+
+
+
