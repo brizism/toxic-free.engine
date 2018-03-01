0db10070cf8fbaae5d18b8fede1283d7e3c55008
brizism <montielbriseida@gmail.com>
2018-03-01 12:46:52 -0500

    update to BEM syntax naming convention

--- js/ui.js
+++ js/ui.js
@@ -9,19 +9,21 @@ class UI {
     console.log(categories);
     this.modal.style.display = 'block';
     let output = `
-      <div class="modal-header">
+      <div class="products-modal__header">
         <span id="close">&times;</span>
-        <h1>${categories[0].product_type.charAt(0).toUpperCase() + categories[0].product_type.slice(1)}</h1>
+        <h1><span>${categories[0].product_type.split('_').join(' ')}</span></h1>
       </div>
+      <div class="products-modal__wrapper">
     `
     // Loop through categories
     categories.forEach(category => {
       output += `
-        <div class="modal-body">
+        <div class="products-modal__body">
           <p>${category.name}</p>
         </div>
       `
     })
+    output += '</div>';
     this.products.innerHTML = output;
 
     this.closeModal();
