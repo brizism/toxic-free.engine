0d3659b410a47fdb06745d4260110c1e31512d70
brizism <montielbriseida@gmail.com>
2018-03-04 19:07:50 -0500

    make second async func for product_category

--- js/makeup.js
+++ js/makeup.js
@@ -1,14 +1,26 @@
 class Makeup {
   constructor(){
-   
+    
   }
 
   // Fetch makeup products from API
   async getMakeup(product_type){
-    const response = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}`);
+    const productsResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}`);
 
-    const responseData = await response.json();
+    const products = await productsResponse.json();
 
-    return responseData;
+    return {
+      products
+    }
+  }
+
+  async getMakeupByCategory(product_category, product_type){
+    const categoryResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}`);
+
+    const categoryProduct = await categoryResponse.json();
+
+    return {
+      categoryProduct
+    }
   }
 }
