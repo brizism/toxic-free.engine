769fd4c396a4b302cb123facf9195ef3db0123e7
brizism <montielbriseida@gmail.com>
2018-03-04 19:49:35 -0500

    async func that fetches by tag

--- js/makeup.js
+++ js/makeup.js
@@ -14,13 +14,23 @@ class Makeup {
     }
   }
 
-  async getMakeupByCategory(product_category, product_type){
+  async getMakeupByCategory(product_type, product_category){
     const categoryResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}`);
 
     const categoryProduct = await categoryResponse.json();
 
     return {
       categoryProduct
+    }
+  }
+
+  async getMakeupByTag(product_type, product_tag){
+    const tagResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_tags=${product_tag}`);
+
+    const tagProduct = await tagResponse.json();
+
+    return {
+      tagProduct
     }
   }
 }
