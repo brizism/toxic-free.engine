5cd421855d084df9fe7ff4843fb4eeb77add232a
brizism <montielbriseida@gmail.com>
2018-03-27 19:10:10 -0400

    catch errors

--- js/makeup.js
+++ js/makeup.js
@@ -5,32 +5,32 @@ class Makeup {
 
   // Fetch makeup products from API
   async getMakeup(product_type){
-    const productsResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}`);
-
-    const products = await productsResponse.json();
-
-    return {
-      products
-    }
+    try {
+      const productsResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}`);
+      const products = await productsResponse.json();
+      return { products };
+    } catch(err) {
+      console.log(err)
+    } 
   }
 
   async getMakeupByCategory(product_type, product_category){
-    const categoryResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}`);
-
-    const categoryProduct = await categoryResponse.json();
-
-    return {
-      categoryProduct
-    }
+    try {
+      const categoryResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}`);
+      const categoryProduct = await categoryResponse.json();
+      return { categoryProduct };
+    } catch(err) {
+      console.log(err)
+    } 
   }
 
   async getMakeupByTag(product_type, product_tag){
-    const tagResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_tags=${product_tag}`);
-
-    const tagProduct = await tagResponse.json();
-
-    return {
-      tagProduct
+    try {
+      const tagResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_tags=${product_tag}`);
+      const tagProduct = await tagResponse.json();
+      return { tagProduct };
+    } catch (err) {
+      console.log(err)
     }
   }
 }
