aa2d9b0a032a07c4fbe3d3b0c5cbcc1004406ba0
brizism <montielbriseida@gmail.com>
2018-04-02 23:23:05 -0400

    edit transport to https

--- js/makeup.js
+++ js/makeup.js
@@ -6,7 +6,7 @@ class Makeup {
   // Fetch makeup products from API
   async getMakeup(product_type){
     try {
-      const productsResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}`);
+      const productsResponse = await fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}`);
       const products = await productsResponse.json();
       return { products };
     } catch(err) {
@@ -16,7 +16,7 @@ class Makeup {
 
   async getMakeupByCategory(product_type, product_category){
     try {
-      const categoryResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}`);
+      const categoryResponse = await fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}`);
       const categoryProduct = await categoryResponse.json();
       return { categoryProduct };
     } catch(err) {
@@ -26,7 +26,7 @@ class Makeup {
 
   async getMakeupByTag(product_type, product_tag){
     try {
-      const tagResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_tags=${product_tag}`);
+      const tagResponse = await fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_tags=${product_tag}`);
       const tagProduct = await tagResponse.json();
       return { tagProduct };
     } catch (err) {
@@ -36,7 +36,7 @@ class Makeup {
 
   async getBoth(product_type, product_category, product_tag){
     try {
-      const productsResponse = await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}&product_tags=${product_tag}`);
+      const productsResponse = await fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?product_type=${product_type}&product_category=${product_category}&product_tags=${product_tag}`);
       const categoryTagProducts = await productsResponse.json();
       return { categoryTagProducts };
     } catch(err) {
